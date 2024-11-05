d2a457aadcc3d8669abd556464bff69641f9b398
Ernest Ng <eyn5@cornell.edu>
2024-11-05 09:38:04 -0500

    Executable in bin directory

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,7 @@
 (**************************************************************************)
 (*  Mini, a type inference engine based on constraint solving.            *)
-(*  Copyright (C) 2006. François Pottier, Yann Régis-Gianas               *)
-(*  and Didier Rémy.                                                      *)
+(*  Copyright (C) 2006. Franï¿½ois Pottier, Yann Rï¿½gis-Gianas               *)
+(*  and Didier Rï¿½my.                                                      *)
 (*                                                                        *)
 (*  This program is free software; you can redistribute it and/or modify  *)
 (*  it under the terms of the GNU General Public License as published by  *)
@@ -20,6 +20,8 @@
 (**************************************************************************)
 
 (* $Id $*)
+
+open Mini
 
 (** Task registration. *)
 let _ = 
